25da28b89bca1dda144845d4fa3eba24a0835671
Olga Diakonova <olga.diakonova@jetbrains.com>
2022-12-28 18:20:35 +0300

    first

---
+++ GetEnvVariables/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+foreach (System.Collections.DictionaryEntry env in System.Environment.GetEnvironmentVariables())
+    Console.WriteLine($"{env.Key}={env.Value}");
